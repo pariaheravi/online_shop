412dd94b1e60fbc28a314c942087fd9f6575add8
paria <paria.heravi@gmail.com>
2020-07-18 16:54:44 +0430

    first commit

--- App.py
+++ App.py
@@ -1,10 +1,10 @@
 import json 
 from json import JSONEncoder
-import sys
-from simplecrypt import encrypt, decrypt
-from base64 import b64encode, b64decode
-from getpass import getpass
-key = 'paria'
+# import sys
+# from simplecrypt import encrypt, decrypt
+# from base64 import b64encode, b64decode
+# from getpass import getpass
+# key = 'paria'
 
 class person:
     def __init__(self, Name, PhoneNumber, Address, Email, Password):
@@ -21,37 +21,51 @@ class person_data(JSONEncoder):
 
 def register():
     name = input('Enter your first name :')
-    phonenumber = input('Enter your Phone number :')
-    address = input('Enter your Address (The province where you live ):')
-    mail = input('Enter your Email/Gmail :')
-    password = b64encode(encrypt(key, input('Enter your Password :'))).decode("utf-8")
+    check(name)  
+    x = check(name)
+    if x == False :
+        print("A user with this name has already beem registered!")
+    else :
+        phonenumber = int(input('Enter your Phone number :'))
+        address = input('Enter your Address (The province where you live ):')
+        mail = input('Enter your Email/Gmail :')
+        password = input('Enter your Password :')
     
+        user = person(name, phonenumber, address, mail, password)
 
-    user = person(name, phonenumber, address, mail, password)
+        # object to JSON
+        person_json = json.dumps(user, cls=person_data)
 
-    # object to JSON
-    person_json = json.dumps(user, cls=person_data)
+         # Json parse
+        person_dict = json.loads(person_json)
 
-    # Json parse
-    person_dict = json.loads(person_json)
+        ##########
+        def write_json(data, filename='users.json'):
+            with open(filename, 'w') as f:
+                json.dump(data, f, indent=4)
+ 
+        with open('users.json') as json_file:
+            data = json.load(json_file)
+            temp = data['users']
 
-##########
-    def write_json(data, filename='users.json'):
-        with open(filename, 'w') as f:
-            json.dump(data, f, indent=4)
+            # python object to be appended
+            y = person_dict
+    
+            # appending data to emp_details
+            temp.append(y)
 
-    with open('users.json') as json_file:
-        data = json.load(json_file)
-        temp = data['users']
-
-        # python object to be appended
-        y = person_dict
+            write_json(data)
+        ##########
         
-        # appending data to emp_details
-        temp.append(y)
+def check(name) :
+    flog = True
+    f = open('users.json','r')
+    data = json.loads(f.read())
+    for i in data["users"] :
+        if name == i["Name"] :
+            flog = False
 
-    write_json(data)
-##########
+    return flog
 
 def logIn():
     userName = input('Enter your username :')
@@ -61,17 +75,26 @@ def logIn():
     data = json.loads(f.read())
 
     for i in data["users"]:
-        password = decrypt(key, b64decode(i['Password'])).decode("utf-8")
-        if userName == i["Name"] and pasS == password:
-            print('Your entry was succesful :)')
-            displayProducts(i["Name"])
-            break
+        # password = decrypt(key, b64decode(i['Password'])).decode("utf-8")
+        if userName == i["Name"] and pasS == i["Password"] :
+            print("Your entry was succesful. ") 
+            yourRequest = input("Enter 'display' to display products , 'orderList' to see the list of orders or 'edit' to edit orders  : ")
+            if yourRequest.lower() == 'display'.lower() :
+                displayProducts(i["Name"])
+                break
+            elif yourRequest.lower() == 'orderList'.lower() :
+                order_list_2(i["Name"])
+                break
+            elif yourRequest.lower() == 'edit'.lower() :
+                edit2(i["Name"])
+                break
     f.close()
 
 class product:
-    def __init__(self,Code,firstName):
+    def __init__(self,Code,firstName , Number):
         self.Code = Code
         self . firstName = firstName
+        self . Number = Number
 
 class product_data(JSONEncoder):
     def default(self, o):
@@ -130,8 +153,16 @@ def pay(yourName) :
             Price2 = i["cost"]
             print(f"your postage : {Price2}")
     file2.close()
+    
+    file4 = open('orders.json' ,'r')
+    data =json.loads(file4.read())
+    for i in data["orders"] :
+        if yourName == i["firstName"] :
+            products_price_2 = products_price * int(i["Number"])
+            break
+    file4.close()
 
-    total = products_price + Price2
+    total = products_price_2 + Price2
     print(f"your total : {total}")
     print(f"Payment was succesfully...!")
     deletOrders(yourName)
@@ -173,20 +204,21 @@ def displayProducts(yourName):
         print(i)
 
     exist = False
-
+     
     code = input("Enter your requset (1.Enter the product code  2.Enter 'cansel'  3. Enter 'payment') :")
-    if code == 'payment' :
+    if code.lower() == 'payment'.lower() :
         pay(yourName)
-    elif code == 'cansel' :
+    elif code.lower() == 'cansel'.lower() :
         cansel(yourName)
     else :
+        Number = input("how many of these products do you want ?")
         for i in data2['Products']:
             if code == str(i["code"]):
                 exist = True
                 break
         f5.close()
         if (exist == True):
-            order = product(code , yourName)
+            order = product(code , yourName ,Number)
             product_json = json.dumps(order, cls=product_data)
 
             product_dict = json.loads(product_json)
@@ -205,7 +237,89 @@ def displayProducts(yourName):
 
                 write_json(data)
 
-class admin:
+def order_list_2(yourName) :
+    f = open('orders.json','r')
+    data = json.loads(f.read())
+    for item in data["orders"] :
+        if yourName == item["firstName"] :
+            print(item)
+            break
+    f.close()
+
+def delete2(yourName) :
+    yourcode = input('Which code ?')
+    f = open('orders.json', 'r')
+    data = json.loads(f.read())
+    a=[]
+    for i in data["orders"]:
+        if  i["Code"] == yourcode and i["firstName"] == yourName :
+            continue
+        else:
+            a.append(i)
+    f.close()
+    
+    product_json = json.dumps(data, cls=product_data)
+    product_dict = json.loads(product_json)
+
+    def write_json(data, filename='orders.json'):
+        with open(filename, 'w') as f:
+            json.dump(data, f, indent=4)
+
+    with open('orders.json') as json_file:
+        data = json.load(json_file)
+        temp = data['orders']
+        temp.clear()
+    for i in a:
+        temp.append(i)
+    write_json(data)
+    f.close()
+
+def edit_orders(yourName) :
+    yourcode = input('Which code ?')
+    f = open('orders.json', 'r')
+    data = json.loads(f.read())
+    a=[]
+    for i in data["orders"]:
+        if  i["Code"] == yourcode and i["firstName"] == yourName :
+            continue
+        else:
+            a.append(i)
+    f.close()
+    
+    Number = input('New number :')
+    firstName = yourName
+    Code = yourcode
+
+    x = {
+        "firstName" : yourName,
+        "Code" : Code,
+        "Number" : Number
+    }
+    
+    def write_json(data, filename='orders.json'):
+        with open(filename, 'w') as f:
+            json.dump(data, f, indent=4)
+
+    with open('orders.json') as json_file:
+        data = json.load(json_file)
+        temp = data['orders']
+        temp.clear()
+        temp.append(x)
+    for i in a:
+        temp.append(i)
+    write_json(data)
+    f.close()
+    
+def edit2(yourName) :
+    your_request = input("what do you want to do delete or edit number ? (edit | delete)")
+    if your_request.lower() == 'edit'.lower() :
+        edit_orders(yourName)
+    elif your_request.lower() == 'delete'.lower() :
+        delete2(yourName)
+    else :
+        print('Error! not found !')
+
+class admin :
     def __init__(self , model , numberInstock , price , code) :
         self . model = model
         self . numberInstock = numberInstock
@@ -244,12 +358,12 @@ def delete():
     write_json(data)
 
 def edit():
-    your_response = input('Which product do you want to edit ? (Code)')
+    yourResponse = input('Which product do you want to edit ? (Code)')
     f = open('product.json', 'r')
     data = json.loads(f.read())
     a = []
     for i in data["Products"]:
-        if  i["code"] == your_response :
+        if  i["code"] == yourResponse :
             continue
         else:
             a.append(i)
@@ -258,7 +372,6 @@ def edit():
     admin_json = json.dumps(data, cls=admin_data)
     admin_dict = json.loads(admin_json)
 
-    x = input('Enter Product that includes model , numberInStock , price ,code : ')
     def write_json(data, filename='product.json'):
         with open(filename, 'w') as f:
             json.dump(data, f, indent=4)
@@ -267,11 +380,12 @@ def edit():
         data = json.load(json_file)
         temp = data['Products']
         temp.clear()
-        temp . append(x)
     for i in a:
         temp.append(i)
     write_json(data)
-    
+
+    newProcudt()
+
 def newProcudt():
     Model = input('Enter the product model :')
     NumberInStock = int(input('Enter the number in stock :'))
@@ -323,16 +437,16 @@ def AdminLogin():
     address = input('Enter the warehouse address :')
     if email == myEmail and PassWord == myPassword and address == warehouseAddress :
         print('You are logged in as an admin :)')
-        x = input('Do You want to create a new product , see a list of orders ,see a list of Products, edit the product or clean product ?(newProduct\orderList\productList\edit\delete) ')
-        if x == 'newProduct':
+        x = input('Do You want to create a new product , see a list of orders ,see a list of Products, edit the product or clean product ?(newProduct|orderList|productList|edit|delete) ')
+        if x.lower() == 'newProduct'.lower():
             newProcudt()
-        elif x == 'orderList' :
+        elif x .lower() == 'orderList'.lower() :
             order_list()
-        elif x == 'productList' :
+        elif x.lower() == 'productList'.lower() :
             product_list()
-        elif x =='edit' :
+        elif x.lower() =='edit'.lower() :
             edit()
-        elif x == 'delete':
+        elif x.lower() == 'delete'.lower():
             delete()
     else:
         print('Invalid !')
@@ -342,11 +456,11 @@ print("Enter the Character 'R' for Register :")
 print("Enter the character 'L' for log in and Order :")
 print("Enter the character 'A' for Admin :")
 request = input("Enter your request :")
-if request == 'R'or request == 'r':
+if request.lower() == 'R'.lower() :
     register()
-elif request == 'L' or request == 'l':
+elif request.lower() == 'L'.lower() :
     logIn()
-elif request == 'A' or request == 'a':
+elif request.lower() == 'A'.lower() :
     AdminLogin()
-else: 
+else : 
     print('Your request is not Valid! try again :')
